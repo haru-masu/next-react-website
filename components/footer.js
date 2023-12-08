7647d59f14fd05758d68489fe4b4ced93887f2bd
haru-masu <yuuto.5853649@gmail.com>
2023-12-08 16:31:10 +0900

    12/08

--- components/footer.js
+++ components/footer.js
@@ -1,9 +1,14 @@
 import Logo from './logo'
+import style from 'styles/footer.module.css'
+import { Footer } from 'components/footer';
 
 export default function Footer() {
     return (
-        <footer>
-            <Logo />
+        <footer className={styles.wrapper}>
+            <div className={styles.flexContainer}>
+                <Logo />
+                [ソーシャル]
+            </div>
         </footer>
     )
 }
--- components/logo.js
+++ components/logo.js
@@ -1,9 +1,8 @@
 import Link from 'next/link'
+import styles from 'styles/logo.module.css'
 
-export default function Logo() {
+export default function Logo({ boxOn = false }) {
     return (
-        <Link href="/">
-            CUBE
-        </Link>
+            <a className={boxOn ? styles.box : styles.basic}>CUBE</a>
     )
 }
