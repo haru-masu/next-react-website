3957ff71f1b2980670b310b751eef5f8665982b6
haru-masu <yuuto.5853649@gmail.com>
2023-12-15 16:25:45 +0900

    12/15

---
+++ components/two-column.js
@@ -0,0 +1,25 @@
+import style from 'styles/two-column.module.css'
+
+export function TwoColumn({ children }) {
+    return (
+        <div className={style.flexContainer}>
+            {children}
+        </div>
+    )
+}
+
+export function TwoColumnMain({ children }) {
+    return (
+        <div className={style.main}>
+            {children}
+        </div>
+    )
+}
+
+export function TwoColumnSidebar({ children }) {
+    return (
+        <div className={style.sidebar}>
+            {children}
+        </div>
+    )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,10 @@
+import Container from 'components/container'
 import Hero from 'components/hero'
 
-export default function Home() {
+export default function Blog() {
   return (
-    <Hero
-      title="CUBE"
-      subtitle="アウトプットしていくサイト"
-      imageOn
-      />
+    <Container>
+      <Hero title="Blog" subtitle="Recent  Posts "/>
+    </Container>
   )
 }
